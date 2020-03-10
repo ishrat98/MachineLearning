bbef03769bd8db90f70a54353d56bdaf25a672a1
emu <bsse0927@iit.du.ac.bd>
2020-03-10 12:27:26 +0600

    bug fixed

--- FaceDetection/facedet.py
+++ FaceDetection/facedet.py
@@ -10,6 +10,6 @@ grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 detectedFaces = casecadeClassifier.detectMultiScale(grayImage)
 
 for(x,y, width, height) in detectedFaces:
-    cv2.rectangle(image, (x,y), (x*width , y*height ), ( 0, 0, 255), 10)
+    cv2.rectangle(image, (x,y), (x+width , y+height ), ( 0, 0, 255), 10)
     
 cv2. imwrite('output.jpg', image)
